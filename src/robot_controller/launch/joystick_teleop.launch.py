509c75a9b86e81fb456e805e8989383ec05e28c0
manueld <manuel.delucchi@iit.it>
2026-06-29 12:21:06 +0200

    Compacted launch file for Xbox SeriesX controller

--- src/robot_controller/launch/joystick_teleop.launch.py
+++ src/robot_controller/launch/joystick_teleop.launch.py
@@ -1,7 +1,5 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-import os
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
@@ -9,21 +7,27 @@ def generate_launch_description():
         package="joy",
         executable="joy_node",
         name="joystick",
-        parameters=[
-            os.path.join(get_package_share_directory("robot_controller"), "config", "joy_config.yaml")
-        ]
+        parameters=[{
+            'device_id': 0,
+            'device_name': "",
+            'deadzone': 0.5,
+            'autorepeat_rate': 20.0,
+            'sticky_button': False,
+            'coalesce_interval_ms': 1
+        }]
     )
 
     joy_teleop = Node(
         package="teleop_twist_joy",
         executable="teleop_node",
         name="teleop_twist_joy",
-        parameters=[{'enable_button': 7,
-                    'axis_linear.x': 1,
-                    'axis_angular.yaw': 0,
-                    'scale_linear.x': 1.0,
-                    'scale_angular.yaw': 1.0,
-                    'publish_stamped_twist': True 
+        parameters=[{
+            'enable_button': 7,
+            'axis_linear.x': 1,
+            'axis_angular.yaw': 0,
+            'scale_linear.x': 1.0,
+            'scale_angular.yaw': 1.0,
+            'publish_stamped_twist': True 
         }],
         remappings=[
             ('cmd_vel', '/robot_controller/cmd_vel')
